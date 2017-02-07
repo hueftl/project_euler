268a59b86808db4bd163c1c729e6e97bf40c56a9
Alexander Menne <alexander.menne@nextgenerationgeek.de>
2017-02-07 21:26:37 +0100

    added rough outline for task 002

---
+++ 002/even_fibonacci_numbers.rs
@@ -0,0 +1,8 @@
+fn main() {
+   // sum up even elements of Fibonacci sequence
+   // which not exceed 4.000.000
+   let (a, b) = (1, 2);
+
+
+   println!("The sum of all even Fibonacci elements with value below 4.000.000 equals to: {}", 0);
+}
