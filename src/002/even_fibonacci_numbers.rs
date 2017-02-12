3d42ea6a669b2d2322a5a9e803c21c182abd1c4b
Alexander Menne <alexander.menne@nextgenerationgeek.de>
2017-02-12 16:42:21 +0100

    added toml file for cargo

---
+++ src/002/even_fibonacci_numbers.rs
@@ -0,0 +1,23 @@
+// struct to hold current and next fib nr
+struct Fibonacci {
+    current: u32,
+    next: u32,
+}
+
+// sequence generator
+fn fibonacci() -> Fibonacci {
+    Fibonacci {
+        current: 1,
+        next: 1,
+    }
+}
+
+//TODO implement the iterator for Fibonacci
+
+fn main() {
+   // sum up even elements of Fibonacci sequence
+   // which not exceed 4.000.000
+   let (a, b) = (1, 1);
+
+   println!("The sum of all even Fibonacci elements with value below 4.000.000 equals to: {}", 0);
+}
