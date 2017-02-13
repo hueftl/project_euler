50074fe22b89597209d09c4a0992cf4683e89a8b
Alexander Menne <alexander.menne@rohde-schwarz.com>
2017-02-13 18:13:10 +0100

    updated problem 2

--- src/bin/p002.rs
+++ src/bin/p002.rs
@@ -1,10 +1,18 @@
-// struct to hold current and next fib nr
 struct Fibonacci {
     current: u32,
     next: u32,
 }
 
-// sequence generator
+impl Iterator for Fibonacci {
+    type Item = u32;
+    fn next(&mut self) -> Option<u32> {
+        let temp = self.current +self.next;
+        self.current = self.next;
+        self.next = temp;
+        Some(self.current)
+    }
+}
+
 fn fibonacci() -> Fibonacci {
     Fibonacci {
         current: 1,
@@ -12,12 +20,11 @@ fn fibonacci() -> Fibonacci {
     }
 }
 
-//TODO implement the iterator for Fibonacci
-
 fn main() {
-   // sum up even elements of Fibonacci sequence
-   // which not exceed 4.000.000
-   let (a, b) = (1, 1);
-
-   println!("The sum of all even Fibonacci elements with value below 4.000.000 equals to: {}", 0);
+    //TODO print sum
+    //let sum = fibonacci()
+    //    .filter(|f| f % 2 == 0) // even elements
+    //    .take_while(|n| n <= &bla) // below 4 mil
+    //    .sum();
+    println!("The sum of all even Fibonacci elements with value below 4.000.000 equals to: {}", sum);
 }
