989dec8b453c621ed64132b1ab48ec3bbbdeb971
Alexander Menne <alexander.menne@nextgenerationgeek.de>
2017-02-16 20:09:06 +0100

    optimized code

--- src/bin/p001.rs
+++ src/bin/p001.rs
@@ -1,14 +1,13 @@
 fn main() {
-    let mut vec = vec![0];
+    let mut sum = 0;
     
     for x in 0..1000 {
         if x % 3 == 0 {
-            vec.push(x);
+            sum += x;
         } else if x % 5 == 0 {
-            vec.push(x);
+            sum += x;
         }
     }
     
-    let vecsum: u32 = vec.iter().sum();
-    println!("The sum of all multiples of 3 and 5 below 1000 equals to {}", vecsum);
+    println!("The sum of all multiples of 3 and 5 below 1000 equals to {}", sum);
 }
